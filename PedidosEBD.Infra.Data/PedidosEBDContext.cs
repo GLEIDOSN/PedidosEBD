8f498f447b797b5342c811508a1b0f5dacc7f6a4
Gleidson Freitas <fc_gleidson@hotmail.com>
2022-09-27 16:38:31 -0300

    Criação da API "Pedidos" Implementação do acesso ao banco de dados com EF Core Implementação da classe de injeção de depedência

---
+++ PedidosEBD.Infra.Data/PedidosEBDContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PedidosEBD.Infra.Data
+{
+    public class PedidosEBDContext : DbContext
+    {
+        public PedidosEBDContext(DbContextOptions options) : base(options)
+        {
+
+        }
+    }
+}
---
+++ PedidosEBD.Infra.IoC/DependencyContainer.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using PedidosEBD.Infra.Data;
+
+namespace PedidosEBD.Infra.IoC
+{
+    public class DependencyContainer
+    {
+        public static void RegisterServices(IServiceCollection services)
+        {
+            services.AddScoped<PedidosEBDContext>();
+        }
+    }
+}
